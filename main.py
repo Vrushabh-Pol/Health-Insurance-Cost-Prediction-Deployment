0001094277ed14f97875c1b6bc9885c7be9bcb33
Vrushabh Pol <126774806+Vrushabh-Pol@users.noreply.github.com>
2023-07-25 23:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,27 +19,22 @@ def main():
     st.write('')
     st.write('')
 
-    #p1 = st.number_input('Enter Your Age:', 1.0, 120.0, step=1.0)
-    p1 = st.slider("Enter Your Age : ",1,120)
+    p1 = st.number_input('Enter Your Age:', 1.0, 120.0, step=1.0)
 
 
-#    s1 = st.selectbox('Enter Your Gender:', ('Male', 'Female'))
-#    if s1 == "Male":
-#        p4 = 1
-#    elif s1 == "Female":
-#        p4 = 0
 
-
-    s1 = st.radio("Select Your Gender : ", ('Male', 'Female'))
+    s1 = st.selectbox('Enter Your Gender:', ('Male', 'Female'))
     if s1 == "Male":
         p4 = 1
     elif s1 == "Female":
         p4 = 0
 
-    p5 = st.number_input('Enter Your BMI Value:', 1.0, 200.0, step=1.0)
 
-    #p6 = st.number_input('How Many Children You Have:', 0.0, 20.0, step=1.0)
-    p6 = st.slider("How Many Children You Have : ",1,10)
+
+    p5 = st.number_input('Enter Your BMI Value:', 1.0, 100.0, step=1.0)
+
+    p6 = st.number_input('How Many Children You Have:', 0.0, 10.0, step=1.0)
+
 
 
     s2 = st.selectbox('Select Your Region:', ('southwest', 'southeast', 'northwest', 'northeast'))
